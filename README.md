0ba6aea37ca96998cf9167f8fb2cfe29162ed7ce
Keethesh <keethesh15@Gmail.com>
2020-02-01 14:55:50 +0400

    Fixed some errors Written README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Hotwire_Adventurer
-Helps you find the name of your Hotwire Hot Deal
+# Hotwire Adventurer
+Being able to find the name of your Hotwire Hot Deal hotel is a superpower you would like to have, but you don't?
+
+Don't worry, this program will do it for you. You'll just need your Hot Deal link, and that's all!
+
+##Requirements
+Python 3
+
+Firefox
+
+A teaspoon of smartness, so that you can decide which hotel will be yours, based on the results.
+
+##Not important things
+**Why is the program named "Hotwire Adventurer" ?**
+
+Because an adventurer discovers the world, and this program discovers your hotel.
--- adventurer.py
+++ adventurer.py
@@ -1,6 +1,8 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import *
 import re
+import webbrowser
 
 opts = Options()
 opts.add_argument("--headless")
@@ -8,13 +10,27 @@ browser = Firefox(options=opts)
 print("The full URL looks like \"https://www.hotwire.com/hotels/details/XXXXXXX\"")
 hotwire_deal_url = input("Enter your full hotwire hot deal URL: ")
 browser.get(hotwire_deal_url)
-room_photo_url = browser.find_element_by_xpath("//div[@class='actual-room-photos__thumbnail "
-                                               "actual-room-photos__thumbnail-blur "
-                                               "actual-room-photos__thumbnail-desktop']").get_attribute("style")
+
+try:
+    room_photo_url = browser.find_element_by_xpath("//div[@ng-class=[$ctrl.thumbnailPhotoContainer]"
+                                                   "actual-room-photos__thumbnail-blur "
+                                                   "actual-room-photos__thumbnail-desktop']").get_attribute("style")
+except NoSuchElementException:
+    room_photo_url = browser.find_element_by_xpath("//div[@class='actual-room-photos__thumbnail "
+                                                   "actual-room-photos__thumbnail-desktop "
+                                                   "actual-room-photos__thumbnail-blur']").get_attribute("style")
+
 room_photo_url = re.findall(r'"([^"]*)"', room_photo_url)[0]
 browser.get("https://www.google.mu/imghp?hl=en&tab=wi&ogbl")
 browser.find_element_by_xpath("//div[@aria-label='Search by image']").click()
 browser.find_element_by_name("image_url").send_keys(room_photo_url)
 browser.find_element_by_xpath("//input[@value='Search by image']").click()
-print("\nVisit " + browser.current_url + " to know your hotel's name!")
+while browser.current_url == "https://www.google.mu/imghp?hl=en&tab=wi&ogbl":
+    pass
+print("The google image webpage will now be opened in your default browser, if this does not work, the link's below:\n" +
+      browser.current_url)
+try:
+    webbrowser.open_new_tab(browser.current_url)
+except:
+    pass
 browser.quit()
